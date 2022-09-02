1c338d9096cfdb23877c1a5f4332d92c518435f8
Harvey <yerik05vh@gmail.com>
2022-09-02 14:38:36 -0500

    added path

--- storedjango/polls/views.py
+++ storedjango/polls/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-  return HttpResponse("Hola mundo")
+  return HttpResponse("Estas en una pagina principal de una App")
+
+
+def detail(request, question_id):
+  return  HttpResponse(f"Estas viendo la pregunta {question_id}")
+
+def results(request, question_id):
+  return  HttpResponse(f"Estas viendo los resultado de la pregunta {question_id}")
+
+def vote(request, question_id):
+  return  HttpResponse(f"Estas votando a la pregunta numero {question_id}")
